3d31bcf7ece015934373b33c549bd00474b82945
hc-hensen <hc@hensen.com.hk>
2024-05-23 12:32:11 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/chart_1A')
+def chart_1():
+    return render_template('chart_1a.html')
+
+@app.route('/chart_2A')
+def chart_2():
+    return render_template('chart_2a.html')
+
+@app.route('/chart_3A')
+def chart_3():
+    return render_template('chart_3a.html')
+
+@app.route('/table_1A')
+def table_1():
+    return render_template('table_1a.html')
+
+@app.route('/table_2A')
+def table_2():
+    return render_template('table_2a.html')
+
+@app.route('/table_3A')
+def table_3():
+    return render_template('table_3a.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
